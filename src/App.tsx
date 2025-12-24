8908d4c7d92fedf84adcc7cf45820a085dda61ff
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-25 00:25:35 +0530

    setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
+import { Link, Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <div>
-       <h1>Hello</h1>
-      </div>
+      
+       
+      <Routes>
+        <Route path="/" element={<NavWrapper/>}>
+        <Route path="/" element={<Home/>}></Route>
+        </Route>
+
+      </Routes>
       
     </>
   )
 }
 
+
+
+
 export default App
+
+
+function NavWrapper(){
+  return <>
+  <nav>
+
+  
+  <Link to='/'>Home</Link>
+  <Link to='/store'>Store</Link>
+  <Link to='/about'>About</Link>
+  </nav>
+
+  </>
+}
---
+++ src/components/Store.tsx
@@ -0,0 +1,10 @@
+
+const Store = () => {
+  return (
+    <div>
+      <h1>Store</h1>
+    </div>
+  )
+}
+
+export default Store
