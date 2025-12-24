84cb1f7f77c89adee7c7ee6be694a2f2e6f81359
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-25 02:27:22 +0530

    setup done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { sum } from "../sum";
 
 const Home = () => {
+
+  const [num,setNum]=useState("")
+
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={() => alert(sum(3, 4))}>Add 3,4</button>
+      <button onClick={() => setNum(sum(3, 4))}>Add 3,4</button>
+      <p>{num}</p>
     </div>
   );
 };
