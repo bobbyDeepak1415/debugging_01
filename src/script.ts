5f0ddde2073ba0e718eea130cc66db77b3d66153
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-27 23:39:21 +0530

    setup done

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,20 @@
 function printName(firstName: string, lastName: string) {
-  console.log(`${firstName} ${lastName}`);
+  console.log(`${firstame} ${lastName}`);
   return `${firstName} ${lastName}`;
 }
 
 function printTimes(n: number, value: string) {
+  debugger;
   for (let i = 0; i <= n; i++) {
     console.log(value);
   }
 }
+
+debugger;
+
+setTimeout(() => {
+  console.log("setTimeout");
+}, 1000);
 
 printName("Leo", "Kingman");
 
