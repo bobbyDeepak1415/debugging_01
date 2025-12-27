8eecba33c18edeec16d42e99da7f963247c7a896
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-27 21:36:09 +0530

    setup done

--- src/script.ts
+++ src/script.ts
@@ -3,3 +3,12 @@ function printName(firstName: string, lastName: string) {
   return `${firstName} ${lastName}`;
 }
 
+function printTimes(n: number, value: string) {
+  for (let i = 0; i <= n; i++) {
+    console.log(value);
+  }
+}
+
+printName("Leo", "Kingman");
+
+printTimes(4, "Bobby");
