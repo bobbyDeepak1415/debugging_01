113b1dda691773f7feaf6fbf438e714135871ccf
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-28 20:23:36 +0530

    setup done

--- src/script.ts
+++ src/script.ts
@@ -1,25 +1,4 @@
-function printName(firstName: string, lastName: string) {
-  console.log(`${firstName} ${lastName}`);
-  return `${firstName} ${lastName}`;
-}
 
-function printTimes(n: number, value: string) {
-  //   debugger;
-  for (let i = 0; i <= n; i++) {
-    console.log(value);
-  }
-}
 
-// debugger;
 
-setTimeout(() => {
-  console.log("setTimeout");
-}, 1000);
 
-printName("Bobby", "Deepak");
-printTimes(4, "Bobby");
-
-document.addEventListener("click", () => {
-  console.log("clicked");
-});
-.
