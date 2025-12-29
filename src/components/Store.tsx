f2f82c8e8b80e2650d17683a71a0b9066e8a20e6
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-30 02:54:50 +0530

    setup done

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 export const Store = () => {
   const [isLoggedIn, setIsloggedIn] = useState(false);
-  // const [msg,setMsg]=useState("")
+
+  const handleClick = () => {
+    setIsloggedIn((prev) => !prev);
+  };
 
   return (
     <div>
       <h1>Store</h1>
 
-      {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
-
+      <button onClick={handleClick}> {isLoggedIn ? "logout" : "login"}</button>
       <br></br>
       <br></br>
+      <p>{isLoggedIn ? "user is loggedin" : "user is loggedOut"}</p>
     </div>
   );
 };
