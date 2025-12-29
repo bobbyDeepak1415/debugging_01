6e8043d92bd10b3d0bbed71912f8f9dbd637464a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-12-30 02:36:29 +0530

    setup done

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
 
 export const Store = () => {
+
+  const [isLoggedIn,setIsloggedIn]=useState(false)
+
   return (
     <div>
       <h1>Store</h1>
+      <button onClick={handleLogin}>Login</button>
+      <br></br>
+      <br></br>
+      <p>{isLoggedIn}</p>
     </div>
   )
 }
