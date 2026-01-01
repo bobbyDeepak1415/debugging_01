cf313a5479a4713b4ebd3b5a28b75665e22eeb66
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-02 00:19:09 +0530

    debugging

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,21 +3,39 @@
 import { useState } from "react";
 
 function Todo() {
+  const [item, setItem] = useState<string>("");
+  const [items, setItems] = useState<string[]>([]);
 
+  const handleClick = () => {
+    setItems((prev) => [...prev, item]);
 
-    const [item,setItem]=useState("")
+    setItem("");
+  };
 
-    const handleClick=()=>{
-        
-    }
+  const handleDelete=()=>{
     
+  }
+
   return (
     <div>
       <h1>Todo</h1>
 
-<input value={item} onChange={(e)=>setItem(e.target.value)}></input>
-<button onClick={handleClick}>Add</button>
+      <input value={item} onChange={(e) => setItem(e.target.value)}></input>
+      <button onClick={handleClick}>Add</button>
+      <div>
+        <ul>
+          {items.map((item, index) => {
+            return <li key={index}>
+                <div>
 
+                <span>{item}</span>
+                <button onClick={()=>handleDelete(index)}>Delete</button>
+                </div>
+
+            </li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
