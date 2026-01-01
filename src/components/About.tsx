500ff48ed5211d8c46feaa60d66ca98f3718fda7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-01 18:16:32 +0530

    debugging

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,38 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const About = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get("https://api.escuelajs.co/api/v1/products");
+
+      const response = res.data;
+
+      setProducts(response);
+      console.log(response);
+    } catch (e) {
+      console.log("failes To fetch", e);
+    }
+  };
+
   useEffect(() => {
-    const handleClick = () => console.log("clicked");
-
-    window.addEventListener("click", handleClick);
-
-    return () => window.removeEventListener("click", handleClick);
+    
+    fetchData();
   }, []);
 
-  return <h1>About</h1>;
+  return (
+    <div>
+      <h1>About</h1>
+
+      <ul>
+        {products.map((product) => {
+          return <li key={product.id}>{product.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default About;
