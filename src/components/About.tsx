dceb5188d75d6deb49f78df4ddd02536ff50ed00
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-01 17:20:05 +0530

    debugging

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,7 @@
 import { useEffect } from "react";
 
 const About = () => {
-  useEffect(() => {
-    setInterval(() => {
-      console.log("memory leaks");
-    }, 1500);
-
-    return () => {
-      // clearInterval(timer)
-    };
-  });
-
+  
   return (
     <div>
       <h1>About</h1>
