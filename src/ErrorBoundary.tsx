436dd23b0c0f96138299a8f4842dba63f8ad4895
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-03 18:27:27 +0530

    debugging

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,26 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasError: false,
-    };
-  }
-
-  static getDerivedStateFromError(error) {
-    return {
-      hasError: true,
-    };
-  }
-
   render() {
-    if (this.state.hasError) {
-      return <h2>something went wrong</h2>;
-    }
-
-    return this.props.children;
+    return (
+      <div>
+        
+      </div>
+    )
   }
 }
 
-export default ErrorBoundary;
+export default ErrorBoundary
